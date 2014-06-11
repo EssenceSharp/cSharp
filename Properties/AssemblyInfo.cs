b736ea2bb4e344cda694cd5b0ef586640183127d
Strategesis <alan.lovejoy@gmail.com>
2014-06-11 14:25:29 -0700

    Changed root directory of the Git repo, so that it includes the entire VS project, and not just source code.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Essence#")]
+[assembly: AssemblyDescription("Implementation of the Essence programming language for the Common Language Runtime environment")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Essence Technologies")]
+[assembly: AssemblyProduct("Essence#")]
+[assembly: AssemblyCopyright("Copyright 2014 Alan Lovejoy. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f0769ff-a619-4b58-8a60-186a13e7c46b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
